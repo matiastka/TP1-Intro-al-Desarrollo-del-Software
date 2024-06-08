89963f1ee8ea3cc8d252df02e2cda874c8a4a1fb
matiastka <matiastka@gmail.com>
2024-06-08 16:22:03 -0300

    V.7.1: Dentro del archivo models.py: 1. Agregamos el codigo para crear la tabla vendedores

--- backend/models.py
+++ backend/models.py
@@ -4,19 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Author(db.Model):
-    __tablename__ = 'authors'
+class Vendedores(db.Model):
+    __tablename__ = 'vendedores'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
-    books = db.relationship("Book")
-
-class Book(db.Model): #se encarga Atu
-    __tablename__ = 'books'
-    id = db.Column(db.Integer, primary_key=True)
-    isbn = db.Column(db.String(255), nullable=False)
-    name = db.Column(db.String(255), nullable=False)
-    cant_pages = db.Column(db.Integer, nullable=False, default=0)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
-    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    nombre_de_usuario = db.Column(db.String(255), nullable=False)
