7d23d769edce18beca39584e9de2d34289a2f5ab
atuel fullana <atuelfullana12@gmail.com>
2024-06-10 14:23:39 -0300

    V8. Agregaciones y modificaciones. 1. Cambiamos el archivo app.py por main.py 2. Agregando las creacion de tablas al iniciar el servidor (usando python3) 3. gitignore ahora ignora la carpeta __pycache__/

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,26 @@
 #Acá va a estar los endpoints de la página (usando flask y flask cors)
-from flask import Flask
-#from models import db
+from flask import Flask, jsonify, request
 
 #from flask_cors import CORS #importamos CORS para que ande el fetch entre 2 páginas
+
+from models import db, Vendedores, Autos
 
 app = Flask(__name__) #denominamos a flask
 port = 5000
 
 #CORS(app) #Con esto van a andar los fetch entre 2 páginas distintas (o una página externa)
 
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://atufullana:misticaroja2011@localhost:5432/db_tp1'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
 @app.route("/") #Si solicitan la homepage del servidor
 def home():
     return "hola mundo"
 
 if __name__ == '__main__':
+    print('Iniciando servidor...')
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', debug=True, port=port)
+    print('Servidor Cerrado...')
